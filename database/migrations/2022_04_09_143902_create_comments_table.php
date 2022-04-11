0d86c18c687ce1f51bb814ef2b06df9b067b6367
Jaycee Mariano <jayceedaily@gmail.com>
2022-04-11 18:43:10 +0800

    Implement comment table migration

---
+++ database/migrations/2022_04_09_143902_create_comments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->foreignId('parent_id')->nullable();
+            $table->string('name', 64);
+            $table->longText('body');
+            $table->timestamps();
+
+            $table->foreign('parent_id')
+                    ->references('id')
+                    ->on('comments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
