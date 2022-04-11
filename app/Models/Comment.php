111ca53cfdb51d19f76921f861755c3f2f48f091
Jaycee Mariano <jayceedaily@gmail.com>
2022-04-11 22:51:39 +0800

    Update Comment model relationship and add level getter

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,17 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'body'];
 
     /**
      * Parent comment
      */
     public function parent()
     {
-        return $this->hasOne(self::class);
+        return $this->hasOne(self::class, 'id', 'parent_id');
     }
 
     /**
@@ -22,7 +25,7 @@ class Comment extends Model
      */
     public function children()
     {
-        return $this->hasMany(self::class);
+        return $this->hasMany(self::class, 'parent_id');
     }
 
     /**
@@ -30,6 +33,39 @@ class Comment extends Model
      */
     public function latestChild()
     {
-        return $this->hasOne(self::class)->latestOfMany();
+        return $this->hasOne(self::class, 'parent_id')->latestOfMany();
+    }
+
+    /**
+     * Get depth of comment
+     */
+    public function getLevel()
+    {
+        return DB::select("
+
+        WITH RECURSIVE cte AS (
+            SELECT
+                1 AS lvl,
+                parent_id,
+                id
+            FROM
+                comments
+
+            UNION ALL
+            SELECT
+                lvl + 1,
+                comments.parent_id,
+                comments.id
+            FROM
+                comments
+                JOIN cte ON comments.parent_id = cte.id
+        )
+
+        SELECT
+            MAX(lvl) AS depth
+        FROM
+            cte
+            where id = ?
+            ;", [$this->id])[0]->depth;
     }
 }
