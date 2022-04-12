5e92b2e55b18849f4efdd2ea71e5aedb8e02ccf9
Jaycee Daily <alohajaycee@gmail.com>
2022-04-12 08:54:18 +0800

    Implement comment level caching

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 
 class Comment extends Model
@@ -41,31 +42,34 @@ class Comment extends Model
      */
     public function getLevel()
     {
-        return DB::select("
+        return Cache::remember('cmt-lvl-' . $this->id, 300, function () {
 
-        WITH RECURSIVE cte AS (
+            return DB::select("
+
+            WITH RECURSIVE cte AS (
+                SELECT
+                    1 AS lvl,
+                    parent_id,
+                    id
+                FROM
+                    comments
+
+                UNION ALL
+                SELECT
+                    lvl + 1,
+                    comments.parent_id,
+                    comments.id
+                FROM
+                    comments
+                    JOIN cte ON comments.parent_id = cte.id
+            )
+
             SELECT
-                1 AS lvl,
-                parent_id,
-                id
+                MAX(lvl) AS depth
             FROM
-                comments
-
-            UNION ALL
-            SELECT
-                lvl + 1,
-                comments.parent_id,
-                comments.id
-            FROM
-                comments
-                JOIN cte ON comments.parent_id = cte.id
-        )
-
-        SELECT
-            MAX(lvl) AS depth
-        FROM
-            cte
-            where id = ?
-            ;", [$this->id])[0]->depth;
+                cte
+                where id = ?
+                ;", [$this->id])[0]->depth;
+        });
     }
 }
