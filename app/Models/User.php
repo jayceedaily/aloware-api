97b76ca57c5d3e533648b302bd599334a396966b
Jaycee Daily <alohajaycee@gmail.com>
2022-05-28 14:02:09 +0800

    Implement followers relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -51,5 +52,25 @@ class User extends Authenticatable
     public function threads()
     {
         return $this->hasMany(Thread::class, 'created_by');
+    }
+
+    /**
+     * Followers
+     *
+     * @return HasManyThrough
+     */
+    public function followers()
+    {
+        return $this->hasManyThrough(self::class, UserFollower::class, 'following_id','id',null,'follower_id');
+    }
+
+    /**
+     * following
+     *
+     * @return HasManyThrough
+     */
+    public function following()
+    {
+        return $this->hasManyThrough(self::class, UserFollower::class, 'follower_id','id',null,'following_id');
     }
 }
