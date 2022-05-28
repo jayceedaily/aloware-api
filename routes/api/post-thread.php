7f46535082aa55270c4a5747d1bbf9bea1aa23cf
Jaycee Daily <alohajaycee@gmail.com>
2022-05-28 16:31:15 +0800

    Remove old route

--- routes/api/post-thread.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\PostThreadController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::get('posts/1/threads', [PostThreadController::class, 'index'])->name('post-thread.index');
-
-Route::post('posts/1/threads', [PostThreadController::class, 'store'])->name('post-thread.create');
